985d7a975757b3c2d71c7300cbe0d69bc03710a7
Dzmitry Sakalouski <Dzmitry_Sakalouski@epam.com>
2018-08-02 15:44:23 +0300

    tracks position

--- geo.js
+++ geo.js
@@ -1,12 +1,35 @@
 window.onload = function() {
+    var lat = document.getElementById("currentPosition-lat");
+    var lon = document.getElementById("currentPosition-lon");
+    var label = document.getElementById("lonlat"); 
     if ("geolocation" in navigator) {
         console.log("YES");
         navigator.geolocation.getCurrentPosition(function(position) {
             console.log(position.coords.latitude, position.coords.longitude);
-            var lat = document.getElementById("currentPosition-lat");
-            lat.innerHTML(position.coords.latitude);
-            var lon = document.getElementById("currentPosition-lon");
-            lon.innerHTML(position.coords.longitude);
+            lat.innerHTML = position.coords.latitude;
+            lon.innerHTML = position.coords.longitude;
         });
+
+        var count = 1;
+
+        function success(pos) {
+            var crd = pos.coords;
+            lat.innerHTML = crd.latitude;
+            lon.innerHTML = crd.longitude; 
+            count++;
+            label.innerHTML = count;
+        }
+
+        function error() {
+            alert(err);
+        }
+
+        var options = {
+            enableHighAccuracy: true,
+            //timeout: 5000,
+            maximumAge: 0
+        };
+
+        var currentNavId = navigator.geolocation.watchPosition(success, error, options);
     }
 }
