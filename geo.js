fb3b20b0778b6d569d07852d827818c456b91bed
Dzmitry Sakalouski <Dzmitry_Sakalouski@epam.com>
2018-08-03 16:38:53 +0300

    kjh

--- geo.js
+++ geo.js
@@ -1,50 +1,40 @@
-window.onload = function() {
-    var lat = document.getElementById("currentPosition-lat");
-    var lon = document.getElementById("currentPosition-lon");
-    var label = document.getElementById("lonlat"); 
-    if ("geolocation" in navigator) {
-        console.log("YES");
-        navigator.geolocation.getCurrentPosition(function(position) {
-            console.log(position.coords.latitude, position.coords.longitude);
-            lat.innerHTML = position.coords.latitude;
-            lon.innerHTML = position.coords.longitude;
-        });
-
-        var count = 1;
-
-        function success(pos) {
-            var crd = pos.coords;
-            lat.innerHTML = crd.latitude;
-            lon.innerHTML = crd.longitude; 
-            count++;
-            label.innerHTML = count;
-        }
-
-        function error(err) {
-            alert(err);
-        }
-
-        var options = {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0
-        };
-
-        var currentNavId = navigator.geolocation.watchPosition(success, error, options);
-    }
-}
 function initMap() {
-    // The location of Uluru
-    var uluru = {lat: 53.908622099999995, lng: 30.3225663};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 15, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position.coords.latitude, position.coords.longitude);
+
+        this.uluru = {lat: position.coords.latitude, lng: position.coords.longitude};
+    
+        this.map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 14, center: this.uluru});
+    
+        this.marker = new google.maps.Marker({position: this.uluru, map: this.map});
+    });
+    // The location of Uluru  
+
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+    
+    navigator.geolocation.watchPosition(success,error,options);
+    
+    function success(pos) {
+        document.getElementById("currentPosition-lat").innerHTML = pos.coords.latitude;
+        document.getElementById("currentPosition-lon").innerHTML = pos.coords.longitude;
+        this.marker = null;
+        this.uluru = {lat: pos.coords.latitude, lng: pos.coords.longitude};
+        this.marker = new google.maps.Marker({position: this.uluru, map: this.map});
+    }
+
+    function error(err) {
+        console.log(err);
+    }
+
+    id = navigator.geolocation.watchPosition(success, error, options);
 }
 
 function getPos() {
-    var that = this;
     navigator.geolocation.getCurrentPosition(function(position) {
         this.lat = position.coords.latitude;
         this.lon = position.coords.longitude;
@@ -52,7 +42,5 @@ function getPos() {
             lat: position.coords.latitude,
             lon: position.coords.longitude
         };
-    });
-
-    
+    });    
 }
