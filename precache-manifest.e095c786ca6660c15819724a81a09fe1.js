0bd565d2b680db65d47bf5746fc88d510677cb5c
DmitrySokolovsky <dmitry_s_i@mail.ru>
2018-12-25 00:26:44 +0300

    build 0.0.1

---
+++ precache-manifest.e095c786ca6660c15819724a81a09fe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73c0a11f4e9b75eeba6b",
+    "url": "/static/js/main.73c0a11f.chunk.js"
+  },
+  {
+    "revision": "fb07ae979a75ff2e0c0c",
+    "url": "/static/js/1.fb07ae97.chunk.js"
+  },
+  {
+    "revision": "73c0a11f4e9b75eeba6b",
+    "url": "/static/css/main.6340c1f8.chunk.css"
+  },
+  {
+    "revision": "273835bd0b1ce9c5cb66b7e8a4b2fb01",
+    "url": "/index.html"
+  }
+];
