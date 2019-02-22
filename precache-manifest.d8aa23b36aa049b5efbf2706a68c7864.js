36ed98b0f93a67b11729c72a645e8660eebd17ae
DmitrySokolovsky <assholedrummer665@yandex.ru>
2019-02-22 14:16:16 +0300

    0.0.4

---
+++ precache-manifest.d8aa23b36aa049b5efbf2706a68c7864.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d67de2def94df47882ae",
+    "url": "/static/js/main.d67de2de.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "d67de2def94df47882ae",
+    "url": "/static/css/main.271be935.chunk.css"
+  },
+  {
+    "revision": "4a45a56e7f97a094ca91e100b1e569e9",
+    "url": "/index.html"
+  }
+];
